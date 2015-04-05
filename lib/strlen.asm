0460cf04d3a9ae48a837b1b2bfcf5bad665741f1
Thorsten Lorenz <thlorenz@gmx.de>
2015-04-05 17:00:53 -0400

    lib: much simpler lib structure
    - using env vars in Makefile to determine which lib to build as executable
    - lib files have a conditional test section at bottom that only activates when env var is set

--- lib/strlen.asm
+++ lib/strlen.asm
@@ -27,3 +27,49 @@ strlen:
   xchg  esi, ecx                ; esi = start, ecx = len
   pop   eax                     ; restore eax
   ret
+
+%ifenv STRLEN
+  %macro _sys_write 2
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, %1
+    mov edx, %2
+    int 80H
+  %endmacro
+
+  section .data
+
+  SAMPLEMSG:   db "0123456789",0
+  SAMPLELEN    equ $-SAMPLEMSG
+  STRLEN       equ SAMPLELEN-1
+
+  FAILMSG:  db "FAILED!",10,0
+  FAILLEN   equ $-FAILMSG
+  PASSMSG:  db "PASSED!",10,0
+  PASSLEN   equ $-PASSMSG
+
+  section .text
+
+  global _start
+
+  _start:
+    nop
+
+    mov esi, SAMPLEMSG
+    call strlen
+
+    cmp ecx, STRLEN
+
+    jz .pass
+
+  .fail:
+    _sys_write FAILMSG, FAILLEN
+    mov ebx, 1
+    jmp .exit
+  .pass:
+    _sys_write PASSMSG, PASSLEN
+    mov ebx, 0
+  .exit:
+    mov eax, 1
+    int 80H
+%endif

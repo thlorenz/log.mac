f2cda079a6051fae1c5c0c2c9d7ddde9287c7e67
Thorsten Lorenz <thlorenz@gmx.de>
2015-04-19 15:21:02 -0400

    nicer string/len labeling

--- example.asm
+++ example.asm
@@ -5,32 +5,32 @@
 global _start
 
 section .text
-  DEBUGMSG:  db "logging a debug message",0
-  DEBUGLEN   equ $-DEBUGMSG
-  INFOMSG:   db "logging an info message",0
-  INFOLEN    equ $-INFOMSG
-  WARNMSG:   db "logging a warn message",0
-  WARNLEN    equ $-WARNMSG
-  ERRORMSG:  db "logging an error message",0
-  ERRORLEN   equ $-ERRORMSG
+  debug  : db "logging a debug message",0
+    .len : equ $-debug
+  info   : db "logging an info message",0
+    .len : equ $-info
+  warn   : db "logging a warn message",0
+    .len : equ $-warn
+  error  : db "logging an error message",0
+    .len : equ $-error
   log_text   ; very important to include this
 
 _start:
   nop
 
   ; supplying length
-  log_debug DEBUGMSG, DEBUGLEN
+  log_debug debug, debug.len
 
-  ; as long as msg is null terminated we can omit length (slightly slower though)
-  log_info  INFOMSG
-  log_warn  WARNMSG
-  log_error ERRORMSG
+  ; as long as  is null terminated we can omit length (slightly slower though)
+  log_info  info
+  log_warn  warn
+  log_error error
 
   xor eax, eax
   mov ah, 10
   mov al, 20
   log_reg eax
-  log_error ERRORMSG
+  log_error error
 
 .exit:
   mov eax, 1
